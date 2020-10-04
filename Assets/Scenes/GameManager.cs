62dd64e4b73b7bd76f4b72dbd7b89ec5db68e75f
Deadbeef <armin.kessler@me.com>
2020-10-04 22:39:36 +0200

    added time freeze

--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -6,6 +6,24 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private void Update() {
+        if(SceneManager.GetActiveScene().name == "StartScreen")    
+            return;
+        
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            togglePause();
+        }
+    }
+
+    private void togglePause()
+    {
+        if(Time.timeScale == 0f)
+            Time.timeScale = 1f;
+        else
+            Time.timeScale = 0f;
+    }
+
     public void startGame()
     {
         Debug.Log("Game Started");
